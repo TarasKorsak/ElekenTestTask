5a0d8cc5454ffe5f61c54569e4e4c1c01fcbd9d6
TarasKorsak <taras_korsak@ukr.net>
2018-04-24 11:32:26 +0300

    for review with sourcemaps

--- assets/scripts/1.js
+++ assets/scripts/1.js
@@ -2,6 +2,8 @@ $( "#table_view" ).click(function(){
 	if ($(".grid_view_wrapper").css('display') !== "none") {
 			$(".grid_view_wrapper").toggle();
 			$(".table_view_wrapper").toggle();
+			$("#grid_view").css("opacity", 1);
+			$("#table_view").css("opacity", 0.4);
 		};      
 	});
 	
@@ -9,7 +11,8 @@ $("#grid_view").click(function(){
         if ($(".table_view_wrapper").css('display') !== "none") {
 			$(".table_view_wrapper").toggle();
 			$(".grid_view_wrapper").toggle();
+			$("#grid_view").css("opacity", 0.4);
+			$("#table_view").css("opacity", 1);
 		};
 	});
         
-       
